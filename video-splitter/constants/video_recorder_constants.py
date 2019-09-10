c472476961d2a02eb3c74e12d3c820f0462a93af
Leonardo Berlatto <leonardo.berlatto@cwi.com.br>
2019-09-09 22:40:33 -0300

    Refactor video recorder

---
+++ video-splitter/constants/video_recorder_constants.py
@@ -0,0 +1,15 @@
+# Standard Video Dimensions Sizes
+STANDARD_DIMENSIONS = {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPES = {
+    'avi': 'XVID',
+    #'mp4': 'H264',
+    'mp4': 'XVID',
+}
