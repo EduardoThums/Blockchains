c522699cf74ea264e25bc633f7871716b2c61402
eduardo.thums <eduardo.thums@cwi.com.br>
2019-09-13 15:01:20 -0300

    Change applications path to the same path

--- bigchaindb/first-network/scripts/clean-util.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-removeContainers(){
-    docker-compose down --volumes --remove-orphan
-}
-
-removeVolumes(){
-    rm -Rf $HOME/docker/volumes/bigchaindb
-}
--- kafka/kafka-producer-consumer-example/src/main/java/br/com/example/kafka/constant/IPFSConstant.java
+++
@@ -1,6 +0,0 @@
-package br.com.example.kafka.constant;
-
-public interface IPFSConstant {
-
-	String IPFS_DAEMON_ADDRESS = "/ip4/127.0.0.1/tcp/5001";
-}
