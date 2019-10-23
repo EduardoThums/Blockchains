2e5daa584a4e647133eda924876c9a94a7227652
Leonardo Berlatto <leoberlatto@gmail.com>
2019-10-22 22:12:13 -0300

    Add api call return with constant camera id for testing

--- video-security-client/src/services/video-excerpt.service.ts
+++ video-security-client/src/services/video-excerpt.service.ts
@@ -1,12 +1,15 @@
 import { BaseService } from './base.service';
 
 export class VideoExcerptService extends BaseService {
+
+	CAMERA_ID: number = 1; 
+
 	constructor() {
 		super('/fabric');
 	}
 
-	async getVideoExcerpt(startInstant: number, endInstant: number) {
-		console.log(`/find-by-camera-id-and-timestamp-range/${1}?startDate=${startInstant}&endDate=${endInstant}`);
+	async getVideoExcerpt(startInstant: number, endInstant: number): Promise<any> {
+		return super.get(`/find-by-camera-id-and-timestamp-range/${this.CAMERA_ID}?startDate=${startInstant}&endDate=${endInstant}`);
 	}
 
 }
