0efe5acb95c3403ae567b91b9fb5e5845f379e45
Leonardo Berlatto <leoberlatto@gmail.com>
2019-11-05 23:19:19 -0300

    Add default file sender for API tests

--- video-splitter/__main__.py
+++ video-splitter/__main__.py
@@ -1,5 +1,6 @@
 from splitter.file_splitter.file_splitter import FileSplitter
 from splitter.recorder.video_recorder import VideoRecorder
+from splitter.video_sender.video_sender import VideoSender
 
 if __name__ == '__main__':
-    FileSplitter('sample.mp4', 10).split()
+    VideoSender.send_file_multiple_times('sample.mp4', 10)
