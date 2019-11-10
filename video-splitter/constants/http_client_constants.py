7bc6d6e349ef0de885f6c65ed5c729a20710655b
eduardo <eduardocristiano01@gmail.com>
2019-11-09 21:16:36 -0300

    Remove note comments

--- video-splitter/constants/http_client_constants.py
+++ video-splitter/constants/http_client_constants.py
@@ -7,20 +7,3 @@ DEFAULT_PARAM_NAME = "file"
 DEFAULT_CAMERA_ID = 1
 
 LOGGER_API_ROUTE = "https://heroku-logger-api.herokuapp.com/log"
-
-# 1s no coded X com a resolucao Y, (1s hd h2bv)
-# fazer tabela,(nao precisa ser redondo) -> unidade de grandeza
-# experimentos -> 4 tecnologias -> video de 1s(tamanho do buffer) -> intervaldo de confianca gera margem de erro e a media
-# media -> erro posistivo e erro negativo
-# horizontal -> tempo em segundos
-# vertical -> quanto demorou
-# analise -> qual a pergunta da pesquisa -> Qual a melhor tecnologia para armazenar
-# para cada um dos experimentos vai ter uma melhor
-# baseado no buffer da camera -> qual a melhor tecnolgia para cada tempo de bugffer(vai gerar outro grafico)
-# segunda pergunta de pesquisa -> qual o por tecnoligia
-# terceira pergunta -> indepenten qual o melhor conjunto dependendo do buffer (trabalhos futuros) analisar em aspectos mais gerais
-# ate quantos segundos a gente vai ir ? R: com base no tempo para buscar
-# Ex: Valores de referencia,
-# 1º Criar tabelas de comparacao (hd, 4k, 720, ultra hd)
-# 2º Definir limite maximo
-# 3º Definir limite minimo
